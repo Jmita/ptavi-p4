c4572884dafb276749e65ea99851380f2c1da62e
Juan Manuel Yuste Carretero <juanmis@iota42.aulas.gsyc.es>
2014-10-24 12:43:47 +0200

    apartado 4 client.py

--- client.py
+++ client.py
@@ -5,15 +5,18 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Cliente UDP simple.
+IP = sys.argv[1]
+Puerto = sys.argv[2]
 
 # Dirección IP del servidor.
 SERVER = 'localhost'
 PORT = 6001
 
 # Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+LINE = sys.argv[3]
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -22,6 +25,9 @@ my_socket.connect((SERVER, PORT))
 
 print "Enviando: " + LINE
 my_socket.send(LINE + '\r\n')
+print IP
+print Puerto
+
 data = my_socket.recv(1024)
 
 print 'Recibido -- ', data
