7f422414801918bce786f6b894bb693e03b1ff80
Juan Manuel Yuste Carretero <juanmis@iota37.aulas.gsyc.es>
2014-10-27 13:08:35 +0100

    apartado 7

--- client.py
+++ client.py
@@ -7,16 +7,14 @@ Programa cliente que abre un socket a un servidor
 import socket
 import sys
 
-# Cliente UDP simple.
-
-
 # Dirección IP del servidor.
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
 REGISTER = sys.argv[3]
 LOGIN = sys.argv[4]
-# Contenido que vamos a enviar
-
+EXPIRE = int(sys.argv[5])
+LINE1 = ""
+LINE2 = ""
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -24,15 +22,12 @@ my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
 if sys.argv[3] == "REGISTER":
-
-        LINE1 = ("REGISTER " + "sip:" + LOGIN + " " + 'SIP/2.0\r\n\r\n')
-
-        my_socket.send(LINE1)
-
-print "Enviando: "
-
+        LINE1 = ("REGISTER " + "sip:" + LOGIN + " " + 'SIP/2.0\r\n')
+        LINE2 = (str(EXPIRE) + '\r\n\r\n')
+my_socket.send(LINE1 + LINE2)
 data = my_socket.recv(1024)
 
+print "Enviando: "
 print 'Recibido -- ', data
 print "Terminando socket..."
 
