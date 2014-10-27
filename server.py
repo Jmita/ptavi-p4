82aaa1e532901141af865338e94769e3a6d6300b
Juan Manuel Yuste Carretero <juanmis@iota37.aulas.gsyc.es>
2014-10-27 13:09:14 +0100

    apartado 7

--- server.py
+++ server.py
@@ -8,27 +8,45 @@ en UDP simple
 import SocketServer
 import sys
 
+dicc = {}
 
-class EchoHandler(SocketServer.DatagramRequestHandler):
+class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
-    Echo server class
+    server class
     """
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write("Hemos recibido tu peticion")
+        self.wfile.write("Hemos recibido tu peticion ")
+        self.wfile.write("SIP/1.0 200 OK\r\n\r\n")
+        IP = self.client_address[0]
+        Puerto = self.client_address[1]
+        print "El cliente nos manda " + IP + (" ") + str(Puerto)
+        # Leyendo línea a línea lo que nos envía el cliente
+        Line = self.rfile.read()
+        Line1 = Line.split()
+        Line2 = Line1[1].split(":")
+        Login = Line2[1]
+        print Line1[3]
+        print Login  
+        dicc[Login] = IP
+        print dicc
         while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
-            #line = self.rfile.read()
-            line = ""
-            IP = self.rfile.read()
-            Puerto = self.rfile.read()
-            print "El cliente nos manda " + IP + Puerto
-            if not line:
+            #if, evalua expire (tiempo) 
+            if Line1[3] == '0':
+                if Login in dicc:
+                    del dicc[Login]
+                    print dicc
+                    self.wfile.write("SIP/1.0 200 OK\r\n\r\n")
+                    break
+            else:
+                self.wfile.write("SIP/1.0 200 OK\r\n\r\n")
+                break
+            if not Line1:
                 break
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = SocketServer.UDPServer(("", 6001), EchoHandler)
+    serv = SocketServer.UDPServer(("", 7001), SIPRegisterHandler)
     print "Lanzando servidor UDP de eco..."
     serv.serve_forever()
